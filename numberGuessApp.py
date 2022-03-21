319e9a3a188e72e659af35c038dc05c5dfc79499
someone who codes in js <80367377+JSCoder165@users.noreply.github.com>
2022-03-21 16:00:42 -0700

    Add files via upload

---
+++ numberGuessApp.py
@@ -0,0 +1,31 @@
+import random
+
+num = random.randint(0, 10)
+print(f'''
+---------------------------
+Correct answer: {num}
+---------------------------
+''')
+
+attempt = 4
+msg = ''
+while attempt > 0:
+   
+    user_input = int(input('Enter Number: '))
+    if user_input == num:
+        msg = 'You Won!'
+        break
+    elif user_input > num:
+        print(f'{user_input} is greater.\nRemaining attempts: {attempt}.')
+        attempt -= 1
+        
+
+    elif user_input < num:
+        print(f'{user_input} is smaller.\nRemaining attempts: {attempt}.')
+        attempt -= 1
+
+    else:
+        print('Something went wrong!')
+        break
+    
+print(msg)
